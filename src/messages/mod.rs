3073b3628e48a0f65440203c284acaae3219daa8
ibotva <etobibillingsley@gmail.com>
2023-09-28 01:19:53 -0400

    Implements boilerplate code for deserializing individual wamp elements. This also introduces serde and serde_json to the Cargo.toml

---
+++ src/messages/mod.rs
@@ -0,0 +1,17 @@
+
+
+pub(crate) mod helpers {
+
+    use std::fmt::Display;
+    use serde::{de::SeqAccess, Deserialize};
+
+    pub(crate) fn deser_seq_element<'de, T: PartialEq + Deserialize<'de>, E: Display, A: SeqAccess<'de>>(seq: &mut A, error: E) -> Result<T, <A as SeqAccess<'de>>::Error> {
+        let element: Option<T> = seq.next_element()?;
+        if element != None {
+            Ok(element.unwrap())
+        } else {
+            Err(serde::de::Error::custom(error))
+        }
+    }
+
+}
