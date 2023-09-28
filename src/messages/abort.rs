12cc1b16fb276bbe2e2cf1d87c4c8f5509542c0f
ibotva <etobibillingsley@gmail.com>
2023-09-28 01:37:01 -0400

    Fix: Corrected to borrow details, and add value_is_object checker.

--- src/messages/abort.rs
+++ src/messages/abort.rs
@@ -1,7 +1,7 @@
 use serde::Serialize;
 use serde_json::Value;
 
-use super::WampMessage;
+use super::{WampMessage, helpers};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Abort {
@@ -17,6 +17,7 @@ impl Serialize for Abort {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: serde::Serializer {
+        let details = helpers::value_is_object::<S, _>(&self.details, "Details must be object like.")?;
         todo!()
     }
 }
--- src/messages/mod.rs
+++ src/messages/mod.rs
@@ -3,7 +3,8 @@ mod abort;
 pub(crate) mod helpers {
 
     use std::fmt::Display;
-    use serde::{de::SeqAccess, Deserialize};
+    use serde::{de::SeqAccess, Deserialize, Serializer, ser, ser::Error};
+    use serde_json::Value;
 
     pub(crate) fn deser_seq_element<'de, T: PartialEq + Deserialize<'de>, E: Display, A: SeqAccess<'de>>(seq: &mut A, error: E) -> Result<T, <A as SeqAccess<'de>>::Error> {
         let element: Option<T> = seq.next_element()?;
@@ -11,6 +12,14 @@ pub(crate) mod helpers {
             Ok(element.unwrap())
         } else {
             Err(serde::de::Error::custom(error))
+        }
+    }
+
+    pub(crate) fn value_is_object<S: Serializer, T: Display>(v: &Value, e: T) -> Result<&Value, S::Error> {
+        if v.is_object() {
+            Ok(v)
+        } else {
+            Err(S::Error::custom(e))
         }
     }
 
